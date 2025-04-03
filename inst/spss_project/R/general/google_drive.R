be4e6f94c2114c76289af72a6be7ce085addc5c5
Caban <caban8@gmail.com>
2025-04-03 10:49:18 +0200

    Separated google import to a single R script

---
+++ inst/spss_project/R/general/google_drive.R
@@ -0,0 +1,29 @@
+
+# google sheets setup -----------------------------------------------------
+
+project_name <- basename(getwd())
+local_path <- paste0(
+  c("dane/surowe/dane_surowe", "dane/metoda/metoda"), ".xlsx"
+) %>%
+  setNames(c("dane_surowe", "metoda"))
+
+files <- tibble(
+  type = names(local_path),
+  local = local_path,
+  g_id = as_id(c(
+    "",
+    ""
+  ))
+)
+
+gs4_auth(Sys.getenv("gmail"))
+drive_auth(Sys.getenv("gmail"))
+
+drive_find(project_name, n_max = 10)
+
+
+files %>%
+  select(file = g_id, path = local) %>%
+  pwalk(drive_download, overwrite = T)
+
+
