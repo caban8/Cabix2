c29e68a01322da60a8fa19f3899ea2fe1c3d44e0
Caban <caban8@gmail.com>
2025-05-23 12:42:07 +0200

    dodałem source'owanie funkcji R/fun

--- inst/spss_project/R/general/startup.R
+++ inst/spss_project/R/general/startup.R
@@ -6,6 +6,8 @@ message("Loading the startup.R script")
 library(magrittr)
 library(yaml)
 
+list.files("R/fun", full.names = TRUE) %>%
+  purrr::walk(source)
 reference <- read_yaml("dane/yaml/reference vectors.yaml")
 reference %>%
   list2env(envir = .GlobalEnv)
