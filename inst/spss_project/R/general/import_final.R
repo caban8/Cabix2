10a6aa1b7d9d3b38ab59981011180c78712cf386
Caban <caban8@gmail.com>
2025-05-31 17:08:33 +0200

    ustawiłem na stałe paths w reference vector do projektu spss

--- inst/spss_project/R/general/import_final.R
+++ inst/spss_project/R/general/import_final.R
@@ -4,20 +4,19 @@
 library(tidyverse)
 
 # Import labels codebook
-codebook <- readxl::read_excel("dane/codebooks/codebook.xlsx")
+codebook <- readxl::read_excel(paths$codebook)
+
 
 
 # Import metoda
-metoda_path <- "dane/metoda/metoda.xlsx"
-metoda_sheets <- readxl::excel_sheets(metoda_path)
+metoda_sheets <- readxl::excel_sheets(paths$metoda)
 metoda <- map(metoda_sheets, readxl::read_excel, path = metoda_path) %>%
   set_names(metoda_sheets)
 hipotezy <- metoda$Hipotezy %>%
   Cabix2::extract_hypotheses(hipoteza, przeformułowanie)
 
 # Import the main data
-Baza_nazwa <- "dane/dane_ostateczne/"
-baza <- haven::read_sav(Baza_nazwa)
+baza <- haven::read_sav(paths$dane_spss)
 baza_fac <- baza %>% mutate(across(where(haven::is.labelled), haven::as_factor))
 
 
