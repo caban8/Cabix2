c9ac5904afb3c07e6cdfa8a99a2819001b3f1895
Caban <caban8@gmail.com>
2025-06-19 14:42:29 +0200

    update question_labels - dodanie pattern do wyboru zmiennych z df

--- R/questionnaire_processing.R
+++ R/questionnaire_processing.R
@@ -11,6 +11,7 @@
 #' If the data frame contains more than one type of data, all values are coerced to character type.
 #'
 #' @param df A data frame
+#' @param pattern A regular expression indicating the prefix of the question labels to be extracted.
 #' @param suffix A regular expression indicating the suffix that needs to be removed from the question labels.
 #' The default value is '\\d+' which matches any digit.
 #' @return A data frame with unique values for each column, grouped by column name
@@ -18,27 +19,44 @@
 #' @examples
 #' df <- data.frame(A = c(1, 2, 3), B = c('a', 'b', 'c'))
 #' question_labels(df)
-question_labels <- function(df, suffix = "\\d+") {
+question_labels <- function(df, pattern, suffix = "\\d+") {
 
   if (!is.data.frame(df)) stop("Input must be a data frame")
+  df <- dplyr::select(df, tidyselect::matches(pattern))
 
-  l_unique <- df %>%
-    purrr::map(unique)
-
-  types <- purrr::reduce(purrr::map(l_unique, typeof), union)
+  l_unique <- pull_unique(df)
+  types <- pull_types_vars(l_unique)
 
   if (length(types) > 1) {
     l_unique <- purrr::map(l_unique, as.character)
     warning("Data frame contains more than one type of data. Coercing all to character")
   }
 
+  unique_to_tibble(l_unique, suffix = suffix)
+}
+
+
+
+# Helper functions --------------------------------------------------------
+
+#' pull unique values from a data frame
+pull_unique <- function(df) purrr::map(df, unique)
+
+#' pull types of variables from a list (containing unique values for specific vars)
+pull_types_vars <- function(l_unique) purrr::reduce(purrr::map(l_unique, typeof), union)
+
+#' Convert a list of unique values to a tibble
+unique_to_tibble <- function(l_unique, suffix = "\\d+") {
   l_unique %>%
     tibble::enframe(name = "Kwestionariusz", value = "Wartosci") %>%
     dplyr::mutate(Kwestionariusz = stringr::str_remove(Kwestionariusz, pattern = suffix)) %>%
-    tidyr::unnest() %>%
+    tidyr::unnest(cols = Wartosci) %>%
     dplyr::group_by(Kwestionariusz) %>%
     dplyr::summarise(
       wartosci = list(sort(unique(Wartosci)))
     ) %>%
-    tidyr::unnest()
+    tidyr::unnest(cols = wartosci)
 }
+
+
+
