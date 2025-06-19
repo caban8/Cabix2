11599e965ae1441183cecc7ed396778bb0257b5c
Caban <caban8@gmail.com>
2025-06-19 19:43:34 +0200

    poprawki paths i inne drobne do project spss

--- inst/spss_project/R/general/import_final.R
+++ inst/spss_project/R/general/import_final.R
@@ -10,7 +10,7 @@ codebook <- readxl::read_excel(paths$codebook)
 
 # Import metoda
 metoda_sheets <- readxl::excel_sheets(paths$metoda)
-metoda <- map(metoda_sheets, readxl::read_excel, path = paths$metoda_path) %>%
+metoda <- map(metoda_sheets, readxl::read_excel, path = paths$metoda) %>%
   set_names(metoda_sheets)
 hipotezy <- metoda$Hipotezy %>%
   Cabix2::extract_hypotheses(hipoteza, przeformułowanie)
