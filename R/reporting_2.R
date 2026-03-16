7c7e5dc1b937e9ebeb9098e90253a72437ca96df
Caban <caban8@gmail.com>
2026-03-16 17:29:30 +0100

    exporting and rejoin ai chat interpreations funs

--- R/reporting_2.R
+++ R/reporting_2.R
@@ -51,3 +51,58 @@ report_i2 <- function(intros, tables, captions, interpretation) {
 
 }
 
+
+
+
+
+
+#' Export AI-chat made interpretations to YAML
+#'
+#' This function exports AI chat interpretations of statistical results from a final report data frame to a YAML file.
+#'
+#' @param report_final A data frame containing the final report data.
+#' @param path The path to save the YAML file.
+#' @param vars A vector specifying the variables to include in the output. Default includes tab_nr, caption, wprowadzenie, and interpretacja.
+#'
+#' @return The path to the saved YAML file.
+#'
+#' @import yaml
+#'
+#' @export
+export_interpretacje <- function(
+    report_final,
+    path,
+    vars = c(tab_nr, caption, wprowadzenie, interpretacja)
+) {
+  report_final |>
+    select({{vars}}) |>
+    yaml::write_yaml(path)
+
+  return(path)
+
+}
+
+
+#' Rejoin AI-chat made interpretations with report final data
+#'
+#' This function joins AI-chat made interpretations data with report final data based on the tab_nr variable.
+#'
+#' @param report_final The report final data frame
+#' @param interpretacje_edited_imported AI-chat made interpretations imported from yaml
+#' @param vars A character vector specifying the variables to select from report_final data frame
+#' @return A tibble with the interpretations and report final data merged together
+#' @export
+rejoin_interpretacje <- function(
+    report_final,
+    interpretacje_edited_imported,
+    vars = c(tab_nr, flextable, wykresy, analiza)
+) {
+  interpretacje_edited_imported |>
+    as_tibble() |>
+    left_join(
+      report_final |> select({{vars}}),
+      by = "tab_nr"
+    )
+}
+
+
